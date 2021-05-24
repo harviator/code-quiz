ba9326a666d15b977f64ea0e086a13ce9d5f7500
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-24 10:31:59 -0400

    pseudo coding

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+//I want a welcome page
+    //On the welcome page I want a start button
+
+//when the start button is pressed
+    //the page is refreshed
+        //the page has a question
+        //the page has 4 buttons that correspond to answers
+        //the page has a timer in the top right corner
+            //the timer:
+                //when timer gets to zero the game stops
+                //the timer is reduced when a question is answered incorrectly - as mentioned below
+        //when a answer is selected:
+            //if the answer is correct:
+                //the score is incremented
+                //stored on local storage
+                //the user can see "Correct!" at the bottom of the page
+            //if the answer is incorrect:
+                //the timer is reduced
+                //the user sees "Incorrect" at the bottom of the page
+            //the page reloads with a new question and answers
+
+//When the game ends:
+    //the user can see their score
+    //the user can put in their initials
+
